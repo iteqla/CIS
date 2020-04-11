7508a9da97273f43bd79deb91dbbdee6b6544030
iteqla <38462266+iteqla@users.noreply.github.com>
2020-04-11 22:28:14 +0100

    Copia incolla della request di esempio.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+
+# primo tentativo
+
 r = requests.get("https://api.chess.com/pub/club/team-toscana/matches")
 toscana_matches = r.json()
 toscana_finished = toscana_matches['finished']
