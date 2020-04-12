15ccf8e091db97fcc142d48aebb693c0f5cb683a
iteqla <38462266+iteqla@users.noreply.github.com>
2020-04-12 01:14:17 +0100

    Aggiunta procedura per l'elenco delle partite

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import requests
 
-# primo tentativo
 
-r = requests.get("https://api.chess.com/pub/club/team-toscana/matches")
-toscana_matches = r.json()
-toscana_finished = toscana_matches['finished']
-print(toscana_finished)
+def toscana_elenco_partite():
+    all_matches = requests.get("https://api.chess.com/pub/club/team-toscana/matches")
+    partite = all_matches.json()
+    finite = partite["finished"]
+    print("\n===== Elenco partite Toscana =====")
+    for x in finite:
+        titolo = x.get("name")
+        risultato = x.get("result")
+        if "CIS2020" in titolo:
+            print(titolo, risultato)
+
+
+
+def abruzzo_elenco_partite():
+    r = requests.get("https://api.chess.com/pub/club/team-abruzzo/matches")
+    partite = r.json()
+    finite = partite["finished"]
+    print("\n===== Elenco partite Abruzzo =====")
+    for x in finite:
+        titolo = x.get("name")
+        risultato = x.get("result")
+        if "CIS2020" in titolo:
+            print(titolo, risultato)
+
+toscana_elenco_partite()
+abruzzo_elenco_partite()
