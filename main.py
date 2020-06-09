9e06d1877817c2710b5ac833216a2045abb74614
Marius <mario@iteqla.com>
2020-06-10 00:34:58 +0100

    Cambio la struttura delle classi. Creata classe Regione

--- main.py
+++ main.py
@@ -1,63 +1,38 @@
 import requests
 torneo = "CIS2020"
+alternativa = "CSI2020"
 
-class Elenco_partite:
-    def __init__(self, nome, request):
+class Regione:
+    def __init__(self, nome):
         self.nome = nome
-        self.request = request
-        self.tutte = request.json()
-        self.finite = self.tutte["finished"]
+        self.giocatori = None
+        self.partite = None
+        self.punteggio = None
 
-    def elenco(self):
+    def elenco_risultati(self, api):
+        self.partite = api.json()
+        self.finite = self.partite["finished"]
         print("\n===== Elenco partite " + self.nome + " =====")
-        for x in self.finite:
-            titolo = x.get("name")
-            risultato = x.get("result")
-            if torneo in titolo:
+        for p in self.finite:
+            titolo = p.get("name")
+            risultato = p.get("result")
+            if torneo in titolo or alternativa in titolo:
                 print(titolo, risultato)
 
+    def elenco_giocatori(self, api):
+        self.giocatori = api.json()
+        self.tutti = self.giocatori["all_time"]
+        print("\n===== Elenco giocatori " + self.nome + " =====")
+        for g in self.tutti:
+            username = g.get("username")
+            print(username)
 
-toscana = Elenco_partite("Toscana", requests.get("https://api.chess.com/pub/club/team-toscana/matches"))
-toscana.elenco()
 
-abruzzo = Elenco_partite("Abruzzo", requests.get("https://api.chess.com/pub/club/team-abruzzo/matches"))
-abruzzo.elenco()
+toscana = Regione("Toscana")
+toscana.elenco_risultati(requests.get("https://api.chess.com/pub/club/team-toscana/matches"))
+toscana.elenco_giocatori(requests.get("https://api.chess.com/pub/club/team-toscana/members"))
 
-emilia = Elenco_partite("Emilia-Romagna", requests.get("https://api.chess.com/pub/club/team-emilia-romagna/matches"))
-emilia.elenco()
+calabria = Regione("Calabria")
+calabria.elenco_risultati(requests.get("https://api.chess.com/pub/club/team-calabria/matches"))
 
-calabria = Elenco_partite("Calabria", requests.get("https://api.chess.com/pub/club/team-calabria/matches"))
-calabria.elenco()
 
-basilicata = Elenco_partite("Basilicata", requests.get("https://api.chess.com/pub/club/team-basilicata/matches"))
-basilicata.elenco()
-
-sicilia = Elenco_partite("Sicilia", requests.get("https://api.chess.com/pub/club/team-sicilia/matches"))
-sicilia.elenco()
-
-# def toscana_elenco_partite():
-#     all_matches = requests.get("https://api.chess.com/pub/club/team-toscana/matches")
-#     partite = all_matches.json()
-#     finite = partite["finished"]
-#     print("\n===== Elenco partite Toscana =====")
-#     for x in finite:
-#         titolo = x.get("name")
-#         risultato = x.get("result")
-#         if torneo in titolo:
-#             print(titolo, risultato)
-#
-#
-#
-# def abruzzo_elenco_partite():
-#     r = requests.get("https://api.chess.com/pub/club/team-abruzzo/matches")
-#     partite = r.json()
-#     finite = partite["finished"]
-#     print("\n===== Elenco partite Abruzzo =====")
-#     for x in finite:
-#         titolo = x.get("name")
-#         risultato = x.get("result")
-#         if torneo in titolo:
-#             print(titolo, risultato)
-#
-# toscana_elenco_partite()
-# abruzzo_elenco_partite()
